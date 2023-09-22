96df04300e56eb8803beba345ade71da9456bb23
stewarterin <erinlu@live.com>
2023-09-21 19:06:22 -0600

    Completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Formats.Asn1;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magic = Console.ReadLine();
+        int magicNum = int.Parse(magic);
+
+        int guessNum = -1;
+        
+        while (guessNum != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guessNum = int.Parse(Console.ReadLine());
+        
+            if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else 
+                Console.WriteLine("You guessed it!");
+        }
     }
 }
