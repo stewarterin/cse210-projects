9e8abf0ec905113b0783b409f3a92e5ab415a25e
stewarterin <erinlu@live.com>
2025-05-07 00:54:00 -0600

    Prep 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+        string grade = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80 && percentage < 90)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70 && percentage < 80)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60 && percentage < 70)
+        {
+            grade = "D";
+        }
+        else if (percentage < 60)
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade: {grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you've passed.");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed. Good luck next time.");
+        }
     }
 }
