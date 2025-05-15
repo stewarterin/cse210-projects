1adbbf5bf987444c77716c390bf4d799360a5bc3
stewarterin <erinlu@live.com>
2025-05-15 02:31:29 -0600

    Journal program- working on menu

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string _menuInput = "";
+
+        while (_menuInput != "5")
+        {
+            Menu myMenu = new Menu();
+            myMenu.DisplayMenu();
+        }
     }
 }
